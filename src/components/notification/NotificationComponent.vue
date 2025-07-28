a4e681c8a614aaf8a516891bbb68fe06b6eda9c9
BENGUERGOURA Oussama <oussamaceltica@gmail.com>
2025-07-28 17:53:10 +0200

    GTS-PRINT#49 : adding notifications
    * Adding NotificationComponent to show localy notification and globaly animated notification via NotificationPlugin.

---
+++ src/components/notification/NotificationComponent.vue
@@ -0,0 +1,77 @@
+<template>
+
+  <div :class="`gts-notification-container ${theme || ''}`">
+    <slot></slot>
+  </div>
+
+</template>
+
+<script>
+
+export default {
+
+  name: 'NotificationComponent',
+
+
+  props: {
+
+    className: {
+      type: String,
+      required: false,
+      default() {
+        return 'gts-badge';
+      }
+    },
+
+    theme: {
+      type: String,
+      required: false,
+      default() {
+        return '';
+      }
+    },
+
+
+  },
+
+}
+</script>
+
+<style lang="scss">
+.gts-notification-container {
+  border: 1px solid;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 0 0.2cm rgba(117, 117, 117, 0.336);
+
+}
+
+/* PRIMARY */
+.gts-notification-primary {
+  color: $primary-color-500;
+  background-color: $primary-color-50;
+  border-color: $primary-color-100;
+
+}
+
+/* SUCESS */
+.gts-notification-success {
+  color: $secondary-color-700;
+  background-color: $secondary-color-100;
+  border-color: $secondary-color-500;
+}
+
+/* DANGER */
+.gts-notification-danger {
+  color: $accent-dark-red-color;
+  background-color: $accent-light-red-color;
+  border-color: $accent-dark-red-color;
+}
+
+/* WARNING */
+.gts-notification-warning {
+  color:rgb(189, 161, 2);
+  background-color: rgb(251, 252, 190);
+  border-color: $accent-dark-orange-color;
+}
+</style>
---
+++ src/components/notification/NotificationExemple.vue
@@ -0,0 +1,59 @@
+<template>
+
+  <div id="exemple-container">
+ 
+    <NotificationComponent :theme="PRIMARY">PRIMARY</NotificationComponent> <br>
+     <NotificationComponent :theme="DANGER">DANGER</NotificationComponent> <br>
+     <NotificationComponent :theme="WARNING">WARNING</NotificationComponent> <br>
+    <NotificationComponent :theme="SUCCESS">SUCCESS</NotificationComponent>
+    
+  </div>
+
+</template>
+
+<script>
+
+
+import { PRIMARY, SUCCESS, WARNING, DANGER } from '@/constants/notifications.js';
+import NotificationComponent from './NotificationComponent.vue';
+
+export default {
+
+  name:"BadgeExample",
+ 
+  components: {
+    NotificationComponent,
+  },
+
+  mounted(){
+   this.$notify(SUCCESS, 'Message de succès', 3000)
+   this.$notify(DANGER, 'Message de succès', 5000)
+   this.$notify(WARNING, 'Message de succès', 7000)
+  },
+
+  data() {
+		return {
+      PRIMARY,
+      SUCCESS,
+      WARNING,
+      DANGER,
+		};
+	},
+
+}
+
+</script>
+
+<style lang="scss">
+
+#exemple-container {
+  display: flex;
+  justify-content: space-around;
+
+  
+}
+
+ 
+
+
+</style>
